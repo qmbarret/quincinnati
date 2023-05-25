e88bf2790efc32769802e1b841e0a01737bb9acd
qmbarret <qmbarret@gmail.com>
2023-05-25 13:39:19 -0600

    Added the temporary register button

--- login.js
+++ login.js
@@ -1,5 +1,10 @@
-async function login() {
+function login() {
     const nameEl = document.querySelector("#username");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "findMatch.html";
-  }  
+}  
+
+function register() {
+    //temporary until Database stuff added
+    login();
+}
