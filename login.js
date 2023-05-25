3ab67d2ec12719ad2009d1f61d867c828dfe5d1d
qmbarret <qmbarret@gmail.com>
2023-05-25 13:32:05 -0600

    Got login to work

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-function login() {
+async function login() {
     const nameEl = document.querySelector("#username");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "findMatch.html";
