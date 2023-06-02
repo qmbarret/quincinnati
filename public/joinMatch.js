0d9f3edc8741eb9c8ae95ce0a197164b5edb5c50
qmbarret <qmbarret@gmail.com>
2023-06-01 23:51:10 -0600

    Fixed issue where username wasn't displayed

--- public/joinMatch.js
+++ public/joinMatch.js
@@ -8,4 +8,15 @@ function joinMatch() {
     const nameEl = document.querySelector("#joinGameID");
     localStorage.setItem("gameID", nameEl.value);
     window.location.href = "play.html";
-}  
+}
+
+function getPlayerName() {
+    let temp = localStorage.getItem('userName');
+    if (temp === null || temp == "") {
+        return "Mystery Player";
+    }
+    return temp;
+}
+
+const playerNameEl = document.querySelector('#playerUsername');
+playerNameEl.textContent = getPlayerName();
