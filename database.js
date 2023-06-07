aeba13c0828ecff526beca76dc8c1c02de623c1c
qmbarret <qmbarret@gmail.com>
2023-06-06 21:28:50 -0600

    Added some basic DB functions

--- database.js
+++ database.js
@@ -3,7 +3,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('quincinnati');
+const gameCollection = db.collection('gameData');
 
 (async function testConnection() {
   await client.connect();
@@ -13,3 +14,27 @@ const db = client.db('rental');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+
+async function addGameData(gameData) {
+    const result = await gameCollection.insertOne(gameData);
+    return result;
+  }
+  
+  function getLeaderboard() {
+    const query = { population: { $gt: 0} };
+    const options = {
+      sort: { score: -1 },
+      limit: 5,
+    };
+    const cursor = gameCollection.find(query, options);
+    return cursor.toArray();
+  }
+
+  function getGameData(checkID) {
+    const query = { gameID: {checkID} };
+    return gameCollection.find(query);
+  }
+  
+  module.exports = { addGameData, getLeaderboard, getGameData };
+  
