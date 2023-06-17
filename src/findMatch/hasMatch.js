66fa4e68fed899c3461e28e8f381d09ba20006d5
qmbarret <qmbarret@gmail.com>
2023-06-17 15:56:03 -0600

    Fixed some layout stuff

--- src/findMatch/hasMatch.js
+++ src/findMatch/hasMatch.js
@@ -1,7 +1,7 @@
 export class HasMatch {
   static Unknown = new HasMatch('unknown');
   static JoinedMatch = new HasMatch('joinedMatch');
-  static NoMatch = new HasMatch('noMatch');
+  static NoMatch = new HasMatch('unauthenticated');
 
   constructor(gameID) {
     this.gameID = gameID;
